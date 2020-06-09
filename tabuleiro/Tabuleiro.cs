2ffe70026e89d2df7bb535338ba53742d092cb99
brenogcota <brenocota4@gmail.com>
2020-06-09 14:14:39 -0300

    add Exceptions

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -12,13 +12,47 @@ namespace tabuleiro {
             pecas = new Peca[linhas, colunas];
         }
 
-        public Peca peca(int linha, int coluna ) {
+        public Peca peca(int linha, int coluna )
+        {
             return pecas[linha, coluna];
         }
 
-        public void colocarPeca(Peca p, Posicao pos) {
+        public Peca peca(Posicao pos) 
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        public void colocarPeca(Peca p, Posicao pos) 
+        {
+            if(existePeca(pos)){
+                throw new TabuleiroException("Ja existe uma peca");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.linha >= colunas)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if(!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posicao invalida");
+            }
         }
     }
 }
